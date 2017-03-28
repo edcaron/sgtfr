98d7decdf3faebc472b59643f72b4537edd71512
Eduardo Caron <eduardocaron10@gmail.com>
2017-03-28 10:59:23 -0300

    war user if the proxy link is not set

--- background.js
+++ background.js
@@ -1,18 +1,47 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
-	
-	//get current url	
+
+	//get current url
 	var tabUrl = tab.url;
-	
+
 	//get the proxy link stored
   	chrome.storage.sync.get({
-    	link: 'https://54.149.70.135/mp/index.php?',
+    	link: '',
   	}, function(items) {
 
-  		//create a new url 
-    	var myNewUrl = items.link + tabUrl;
-	
-		//Update the current url to the proxyed url
-		chrome.tabs.update(tab.id, {url: myNewUrl});
+
+      if(items.link != ""){
+    		//create a new url
+      	var myNewUrl = items.link + tabUrl;
+
+    		//Update the current url to the proxyed url
+    		chrome.tabs.update(tab.id, {url: myNewUrl});
+      }else{
+          alert("You have to set the proxy server first. Go to the preferences page and fill the form")
+      }
+
   	});
-  	
+
 });
+
+openWithProxy = function(word){
+    var query = word.linkUrl;
+	  //get the proxy link stored
+  	chrome.storage.sync.get({
+    	link: '',
+  	}, function(items) {
+
+  		//create a new url
+    		var myNewUrl = items.link + query;
+
+    		//create a new tab with the proxyed url
+    		chrome.tabs.create({url: myNewUrl});
+  	});
+
+};
+
+chrome.contextMenus.create({
+ title: "Proxy it for me!",
+ contexts:["link"],  // ContextType
+ onclick: openWithProxy // A callback function
+});
+
